09450bdc6f66d5be302dbd8218e2cba20585f1ba
ameedasmah <ameed.asmah@hotmail.com>
2021-06-17 16:45:27 +0300

    AuthorPublisher

---
+++ Contract/resource/PublisherResourse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Contract.Resourse
+{
+    public class PublisherResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<PublisherBookCreate> Books { get; set; }
+    }
+}
---
+++ Domain/mangers/Producer/PublisherSend.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.mangers.Producer
+{
+    public class SendArgument
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+
+    public interface IPublisherSend
+    {
+        void sendPublisher(SendArgument Publisher);
+    }
+    public class PublisherSend : IPublisherSend
+    {
+        public void sendPublisher(SendArgument Publisher)
+        {
+            try
+            {
+                var connectionFactory = new ConnectionFactory()
+                {
+                    HostName = "localhost",
+                    UserName = "guest",
+                    Password = "guest",
+                    Port = 5672,
+                };
+                using (var rabbitConnection = connectionFactory.CreateConnection())
+                using (var channel = rabbitConnection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "publisher", durable: true, exclusive: false, autoDelete: false, arguments: null);
+                    var json = JsonConvert.SerializeObject(Publisher);
+                    var body = Encoding.UTF8.GetBytes(json);
+                    channel.BasicPublish(exchange: "", routingKey: "publisher", basicProperties: null, body: body);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Check the connection : {ex.Message}");
+            }
+        }
+    }
+}
